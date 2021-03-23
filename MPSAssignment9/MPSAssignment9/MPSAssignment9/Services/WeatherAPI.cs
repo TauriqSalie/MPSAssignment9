99d3ab3e67038b27ca5518e6eb0d3f681256218f
Tauriq Salie <mtauriq.salie@gmail.com>
2021-03-23 07:34:48 +0200

    Created weather details page in models, weatherAPI page in services and weather app view model in view models on xamarin

---
+++ MPSAssignment9/MPSAssignment9/MPSAssignment9/Services/WeatherAPI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using MPSAssignment9.Models;
+using Newtonsoft.Json;
+
+namespace MPSAssignment9.Services
+{
+    class WeatherAPI
+    {
+        public const string WEATHERMAPAPIKEY = "0073d637b8d626a0404feb89fac3e2a1";
+        public const string BASE_URL = "https://api.openweathermap.org/data/2.5/onecall?lat=-33.9249&lon=18.4241&units=metric&appid=0073d637b8d626a0404feb89fac3e2a1";
+        public static async Task<OneCallAPI> GetOneCallAPIAsync(double lat, double lon, string units)
+        {
+            OneCallAPI weather = new OneCallAPI();
+            string url = String.Format(BASE_URL, lat, lon, units, WEATHERMAPAPIKEY);
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var post = JsonConvert.DeserializeObject<OneCallAPI>(content);
+                weather = post;
+            }
+            return weather;
+        }
+
+    }
+}
+
---
+++ MPSAssignment9/MPSAssignment9/MPSAssignment9/View-Models/WeatherAppViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using MPSAssignment9.Services;
+using MPSAssignment9.Models;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace MPSAssignment9.View_Models
+{
+    public class WeatherAppViewModel
+    {
+        private IList<OneCallAPI> _weatherDetails;
+        public IList<OneCallAPI> WeatherDetails
+        {
+            get
+            {
+                if (_weatherDetails == null)
+                    _weatherDetails = new ObservableCollection<OneCallAPI>();
+                return _weatherDetails;
+            }
+            set
+            {
+                _weatherDetails = value;
+            }
+        }
+      
+        private async Task APIAsync()
+        {
+            var weather = await WeatherAPI.GetOneCallAPIAsync(-33.9249, 18.4241, "metric");
+            WeatherDetails.Add(weather);
+        }
+        public WeatherAppViewModel()
+        {
+            Task.Run(APIAsync);
+        }
+    }
+}
+
+    
+
+
+        
+
+
